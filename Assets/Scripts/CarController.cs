d7ac1e22ba642fc11499f9a08f6afd1753e4ece6
Gabby <galbrecht28@gmail.com>
2025-07-19 12:12:35 -0400

    playing with car rotation

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,11 +6,12 @@ public class CarController : MonoBehaviour
     public float baseSpeed = 20f;
     public float topSpeed = 200f;
     public Vector3 currentSpeed;
-    public float rotationSpeed = 35f;
-    public float topRotationSpeed = 50f;
+    public float rotationSpeed = 5f;
+    public float topRotationSpeed = 8f;
     public Vector3 currentRotationSpeed;
     public float horizontalInput;
     public float forwardInput;
+    public bool isDrifting;
     public Vector3 InputKey;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,19 +26,22 @@ public class CarController : MonoBehaviour
         // Get user input
         horizontalInput = Input.GetAxisRaw("Horizontal");
         forwardInput = Input.GetAxisRaw("Vertical");
+        isDrifting = Input.GetKey(KeyCode.LeftShift);
+
+        // Set linear velocity on this car
+        Vector3 linearVelocity = body.linearVelocity + (transform.forward * forwardInput * baseSpeed * Time.deltaTime);
+        linearVelocity = isDrifting ? linearVelocity : Vector3.Dot(transform.forward, linearVelocity) * transform.forward;
+        body.linearVelocity = Vector3.ClampMagnitude(linearVelocity, topSpeed);
+
+        currentSpeed = body.linearVelocity;
 
         // Set angular velocity on this car
+        // Determine how fast rotation speed should be based on current speed
+        // rotationSpeed = 
         Vector3 angularVelocity = body.angularVelocity + (Vector3.up * horizontalInput * rotationSpeed * Time.deltaTime);
         body.angularVelocity = Vector3.ClampMagnitude(angularVelocity, topRotationSpeed);
 
         currentRotationSpeed = body.angularVelocity;
-
-        // Set linear velocity on this car
-        Vector3 linearVelocity = body.linearVelocity + (transform.forward * forwardInput * baseSpeed * Time.deltaTime);
-        linearVelocity = Vector3.Dot(transform.forward, linearVelocity) * transform.forward;
-        body.linearVelocity = Vector3.ClampMagnitude(linearVelocity, topSpeed);
-        
-        currentSpeed = body.linearVelocity;
 
         // InputKey = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
