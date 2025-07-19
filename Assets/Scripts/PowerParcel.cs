7c5290acd9c316ba49ffa75921982025eed659d8
Gabby <galbrecht28@gmail.com>
2025-07-19 16:55:41 -0400

    add partocles when you pick up parcel

--- Assets/Scripts/PowerParcel.cs
+++ Assets/Scripts/PowerParcel.cs
@@ -4,6 +4,7 @@ public class PowerParcel : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip[] collectSounds;
+    public ParticleSystem PowerBurstPreFab;
     public int PowerToGive = 100;
     
     private void OnTriggerEnter(Collider other)
@@ -12,6 +13,9 @@ public class PowerParcel : MonoBehaviour
         {
             if(GameManager.Instance != null)
              GameManager.Instance.Kilowatts += PowerToGive;
+
+            ParticleSystem particles = Instantiate(PowerBurstPreFab);
+            particles.transform.position = other.transform.position;
 
             SoundsOnObjectDestroy.instance.PlayCollect();
             Destroy(gameObject);
